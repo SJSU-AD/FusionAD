2fbe97ca56a6ae3ecc3db3a3589e0a6090156f49
Menson Li <menson168@gmail.com>
2018-05-28 00:01:35 -0700

    Added lat controller and velodyne pkg

---
+++ src/modules/control/include/control/lat_controller.h
@@ -0,0 +1,21 @@
+#ifndef LAT_CONTROLLER_H
+#define LAT_CONTROLLER_H
+
+//Declare all used external libraries
+
+
+//Namespace declaration
+namespace lat_controller
+{
+  class stanley
+  {
+    public:
+      stanley();
+      ~stanley();
+      void initRosComm();
+    private:
+      
+  };
+}
+
+#endif  
