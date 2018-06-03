bc9fe0cd135e3e11d4122974d6e830892bc5dbb9
Menson Li <menson168@gmail.com>
2018-06-03 15:36:24 -0700

    Added buid script, and edited readme.md for instruction

---
+++ ext_package_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~
+sudo mkdir libsbp
+cd libsbp
+git clone https://github.com/swift-nav/libsbp
+cd libsbp/c
+mkdir build
+cd build
+cmake ../
+make
+sudo make install   # install headers and libraries into /usr/local
