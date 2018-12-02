866c166429972f4ad5c26138edc748dd9903a5a6
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2018-12-02 01:23:43 -0800

    Prep for geodesy modifications

--- src/modules/geodesy/test/test_geodesy.py
+++ src/modules/geodesy/test/test_geodesy.py
@@ -28,15 +28,62 @@ class TestGeodesy(unittest.TestCase):
     
     def tearDown(self):
         unittest.TestCase.tearDown(self)
-
-    def test_euclidian_distance_2d_perfect_square(self):
-            self.assertAlmostEqual(self.geodesyTester.euclidian_distance_2d(0, 0, 3, 4), 5, places=7)
     
+    # TODO: Implement type safety in accordance to issue #133
+    #############################
+    ##### Read/Write Access #####
+    #############################
+    def test_read_access_a(self):
+        pass
+
+    def test_read_access_b(self):
+        pass
+
+    def test_read_access_e(self):
+        pass
+
+    def test_read_access_f(self):
+        pass
+
+    def test_read_access_eSquared(self):
+        pass
+    
+    def test_write_access_a(self):
+        pass
+
+    def test_write_access_b(self):
+        pass
+
+    def test_write_access_e(self):
+        pass
+
+    def test_write_access_f(self):
+        pass
+
+    def test_write_access_eSquared(self):
+        pass
+    
+    #######################################################
+    ##### euclidian_distance_2d(self, x1, y1, x2, y2) #####
+    #######################################################
     def test_euclidian_distance_2d_zeros(self):
         self.assertEqual(self.geodesyTester.euclidian_distance_2d(0, 0, 0, 0), 0)
 
-    # def test_euclidian_distance_3d(self):
-    #     self.assertEqual(1, 2)
+    def test_euclidian_distance_2d_perfect_square(self):
+        self.assertAlmostEqual(self.geodesyTester.euclidian_distance_2d(0, 0, 3, 4), 5, places=7)
+
+    def test_euclidian_distance_2d_large_value(self):
+        self.assertAlmostEqual(self.geodesyTester.euclidian_distance_2d(0, 0, 9999999999, 9999999999), 14142135622.316736, places=7)
+    
+    ###############################################################
+    ##### euclidian_distance_2d(self, x1, y1, z1, x2, y2, z2) #####
+    ###############################################################
+    def test_euclidian_distance_3d_zeros(self):
+        self.assertEqual(self.geodesyTester.euclidian_distance_3d(0, 0, 0, 0, 0, 0), 0)
+    
+    def test_euclidian_distance_3d_large_value(self):
+        """One order of magnitude greater than diameter of Earth, in meters"""
+        self.assertAlmostEqual(self.geodesyTester.euclidian_distance_3d(0, 0, 0, 99999999, 99999999, 99999999), 173205079.02484, places=5)
     
 def main():
     import rostest
