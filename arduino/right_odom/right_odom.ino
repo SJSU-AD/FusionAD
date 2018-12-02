00b1ebb4e66d030003c2d606720bad42a61c4a38
joeyzhu00 <joeyzhu00@gmail.com>
2018-12-01 16:25:48 -0800

    ISS 123 (#135)
    * Iss 120 (#121)
    
    * Refactored Localization folders and made (potential) gpf fix
    
    * Fixed file endings
    
    * Working on updating normal calculation method
    
    * New normal estimation method
    
    * New Gpf method
    
    * Gpf final cleanup and stable build
    
    * Added comments to segmenter_node
    
    * Kalman Filter Code
    
    * Removed unused Kalman Filter code
    
    * Moving Median Filter Wheel Odometry (#117)
    
    * yes
    
    yes
    
    * Switched control to run on imu yaw, implemented tf from imu frame to ENU w/ consideration on magnetic declination
    
    * Magnetic_declination angle fix
    
    * Build Error Fix
    
    * Changed imu tf from 105 deg to 90 deg
    
    * Updated ardu_adapter.py structure and included TODO to address subscriber frequency issue with steering operation
    
    * Update changes
    
    * Adding Wheel Odometry node along with velocity covariance collection python script. Arduino code included for getting data from SIGNWISE 600 pulse/rev quadrature encoder. Monitoring change in pulse for interrupts for four times the resolution (2400 pulse/rev).
    
    * Update src/modules/control/src/ardu_adapter.py
    
    Co-Authored-By: joeyzhu00 <joeyzhu00@gmail.com>
    
    * Added Loam and test_gsl into repo
    
    * Editing the ext_package_build.sh file
    
    * Fixing commenting error in the ardu_adapter.py code and inclusion of robot_localization package in the utility folder
    
    * Removed Robot local pkg
    
    * Putting robot_localization pkg back in
    
    * Iss 104 (#112)
    
    * Switched control to run on imu yaw, implemented tf from imu frame to ENU w/ consideration on magnetic declination
    
    * Magnetic_declination angle fix
    
    * Build Error Fix
    
    * Added new installation steps to bash script, added new message def for planning
    
    * Naming convention Clean up for control stack, revised debugging data collection mechanism
    
    * Update src/modules/interface/msg/Waypoint.msg
    
    Co-Authored-By: menson168 <menson168@gmail.com>
    
    * Fixed zero gain bug, tested in unit test
    
    * Changed Test code to using near for decimal prceision, Test ran and passed all cases
    
    * Modify localization support scripts
    
    * Including a moving median filter to the wheel odometry
    
    * Adding support script for the EKF package to conform to ROS sensor_msgs standards in a yaw=0 pointing East ENU frame.
    
    * Cleaning up file names
    
    * Changing file names and limited the serial bus load in terms of bit rate by changing data types used
    
    * Changing Moving Median Filter to Median Filter
    
    * Work in progress for ISS-123
    
    * Addition of the yaw calibration to the Wheel Odometry code

---
+++ arduino/right_odom/right_odom.ino
@@ -0,0 +1,60 @@
+#include <ros.h>
+#include <std_msgs/Int32.h>
+ros::NodeHandle nh;
+std_msgs::Int32 right_odometry_feedback;
+// NOTE: CURRENT CODE ON THIS IS NOT UPLOADED TO THE ARDUINO AT THE MOMENT
+// library for high performance reads and writes by jrraines
+// see http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1267553811/0
+// and http://code.google.com/p/digitalwritefast/
+
+#include <digitalWriteFast.h>
+ros::Publisher right_odometry_publisher("/localization/right_encoder_reading", &right_odometry_feedback);
+
+// right right_encoder
+// Red - 5V
+// Black - GND
+const int right_encoder_a = 2; // Green - pin 2 - Digital
+const int right_encoder_b = 3; // White - pin 3 - Digital
+long right_encoder = 0;
+void setup()
+{
+  // set the baud rate at 115200 baud
+  nh.getHardware()->setBaud(115200);
+  // initialize the ros node
+  nh.initNode();
+  
+  nh.advertise(right_odometry_publisher);
+  // Quadrature right_encoders
+  // right right_encoder
+  pinMode(right_encoder_a, INPUT_PULLUP);
+  pinMode(right_encoder_b, INPUT_PULLUP);
+
+  attachInterrupt(0, right_encoderPinChangeA, RISING);
+  attachInterrupt(1, right_encoderPinChangeB, RISING);
+
+}
+
+void loop()
+{
+  nh.spinOnce();
+
+  right_odometry_feedback.data = right_encoder;
+  right_odometry_publisher.publish(&right_odometry_feedback);
+
+  delay(20);
+}
+
+// Interrupt service routines for the right motor's quadrature right_encoder
+void right_encoderPinChangeA()
+{
+  //right_encoder += digitalReadFast(right_encoder_a) == digitalReadFast(right_encoder_b) ? -1 : 1;
+  right_encoder += digitalReadFast(right_encoder_a) != digitalReadFast(right_encoder_b) ? -1 : 1;
+}
+
+void right_encoderPinChangeB()
+{
+  //right_encoder += digitalReadFast(right_encoder_a) != digitalReadFast(right_encoder_b) ? -1 : 1;
+  right_encoder += digitalReadFast(right_encoder_a) == digitalReadFast(right_encoder_b) ? -1 : 1;
+}
+
+
