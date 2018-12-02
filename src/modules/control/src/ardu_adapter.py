31ba17a9ce9555b6f136b794278a9270e0f417af
DRP <donrobert.pornaras@gmail.com>
2018-12-01 16:28:30 -0800

    Revert "ISS 123 (#135)"
    This reverts commit 00b1ebb4e66d030003c2d606720bad42a61c4a38.

--- src/modules/control/src/ardu_adapter.py
+++ src/modules/control/src/ardu_adapter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import rospy
-#from std_msgs.msg import Float64
-from std_msgs.msg import Int16
+from std_msgs.msg import Float64
 from interface.msg import Controlcmd
 '''
 NOTE: Please be in ~/FusionAD to run this code with the following commands:
@@ -20,8 +19,8 @@ Eventually this script will be used for low level control when we move away from
 TODO: Need to prevent steering oscillation due to slower subscriber rate than 50 Hz
 '''
 # declaring publishers for propulsion and steering
-steering_low_lvl_node_publisher = rospy.Publisher('/control/steering_channel', Int16, queue_size=10)
-driving_low_lvl_node_publisher = rospy.Publisher('/control/driving_channel', Int16, queue_size=10)
+steering_low_lvl_node_publisher = rospy.Publisher('/control/steering_channel', Float64, queue_size=10)
+driving_low_lvl_node_publisher = rospy.Publisher('/control/driving_channel', Float64, queue_size=10)
 
 # declaring constants
 desired_steering = 319
@@ -29,9 +28,12 @@ steering_msg = 0
 desired_throttle = 0
 driving_msg = 0
 
+pi = 3.1415926535
+
+potentiometer_offset = 112
+steering_analog_slope = .1721
+steering_analog_intercept = 36.15
 steering_limit_radians = 0.33
-steering_analog_slope = 424.2424
-steering_analog_intercept = 322
 
 min_driving_input = 0
 max_driving_input = 100
@@ -46,7 +48,10 @@ def high_lvl_callback(high_lvl_control_msg):
     steering_msg = high_lvl_control_msg.steeringAngle
     global driving_msg
     driving_msg = high_lvl_control_msg.throttle
-    
+
+def timer_callback(event):
+    '''Handles the conversion between steering in radians from the high level control and analog value for the low level control
+    '''
     global desired_throttle
     if driving_msg >= 0:
         desired_throttle = (driving_msg-min_driving_input)*(max_driving_output-min_driving_output)/(max_driving_input-min_driving_input)+min_driving_output
@@ -60,30 +65,11 @@ def high_lvl_callback(high_lvl_control_msg):
         steering_angle = steering_msg 
 
     global desired_steering
-    desired_steering = ((-1)*steering_angle*steering_analog_slope+steering_analog_intercept)
+    desired_steering = ((-1)*steering_angle*180/pi+steering_analog_intercept)/steering_analog_slope+potentiometer_offset
+    
     steering_low_lvl_node_publisher.publish(desired_steering)
     driving_low_lvl_node_publisher.publish(desired_throttle)
-    
-    
-# def timer_callback(event):
-#     '''Handles the conversion between steering in radians from the high level control and analog value for the low level control
-#     '''
-#     global desired_throttle
-#     if driving_msg >= 0:
-#         desired_throttle = (driving_msg-min_driving_input)*(max_driving_output-min_driving_output)/(max_driving_input-min_driving_input)+min_driving_output
-#     else:
-#         desired_throttle = (-1)*(abs(driving_msg)-min_driving_input)*(max_driving_output-min_driving_output)/(max_driving_input-min_driving_input)+min_driving_output
-#     steering_angle = 0
 
-#     if(abs(steering_msg) > steering_limit_radians): # establishing limits in radians for steering
-#         steering_angle = steering_limit_radians*(abs(steering_msg)/steering_msg)
-#     else:
-#         steering_angle = steering_msg 
-
-#     global desired_steering
-#     desired_steering = ((-1)*steering_angle*steering_analog_slope+steering_analog_intercept)
-#     steering_low_lvl_node_publisher.publish(desired_steering)
-#     driving_low_lvl_node_publisher.publish(desired_throttle)
 
 def Control_Adapter_Arduino_Node():
     '''Node to facilitate the publisher and subscriber relationship between the high lvl and low lvl control
@@ -91,20 +77,18 @@ def Control_Adapter_Arduino_Node():
     rospy.init_node('Control_Adapter', anonymous = True)
 
     rospy.Subscriber("/control/controlcmd", Controlcmd, high_lvl_callback)
-    # timer = rospy.Timer(rospy.Duration(0.02), timer_callback)
+    timer = rospy.Timer(rospy.Duration(0.02), timer_callback)
 
-    # rospy.spin()
-    # timer.shutdown()
-    # '''
-    #rate = rospy.Rate(50)
-    rate = rospy.Rate(25)
+    rospy.spin()
+    timer.shutdown()
+    '''
+    rate = rospy.Rate(50)
 
     while not rospy.is_shutdown():
         steering_low_lvl_node_publisher.publish(desired_steering)
         driving_low_lvl_node_publisher.publish(desired_throttle)
         rate.sleep()
-    
-    
+    '''
 
 if __name__ == '__main__':
     try:
--- src/modules/localization/src/wheel_odom/Velocity_Estimation.py
+++ src/modules/localization/src/wheel_odom/Velocity_Estimation.py
@@ -12,25 +12,23 @@ previous_right_encoder_count = 0
 left_encoder_count = 0
 previous_left_encoder_count = 0
 
-# left_angular_vel = 0
-# right_angular_vel = 0
-left_pos = 0
-right_pos = 0
+left_angular_vel = 0
+right_angular_vel = 0
 vel_magnitude = 0
 
 PI = 3.14159
-WHEEL_RADIUS = 0.1397
-PULSES_PER_ROTATION = 1200
+WHEEL_RADIUS = 0.7112
+PULSES_PER_ROTATION = 2400
 DT = 0.02
 
 # initialize the publisher
-velocity_estimate_publisher = rospy.Publisher('/localization/velocity_estimate', Float32, queue_size = 10)
+velocity_estimate_publisher = rospy.Publisher('/localization/velocity', Float32, queue_size = 10)
 
 def right_encoder_callback(right_encoder_msg):
     '''callback function for the right encoder
     '''
     global right_encoder_count
-    right_encoder_count = (-1)*right_encoder_msg.data
+    right_encoder_count = right_encoder_msg.data
 
 def left_encoder_callback(left_encoder_msg):
     '''callback function for the left encoder
@@ -44,31 +42,18 @@ def vel_timer_callback(event):
     # declare the globals for this function
     global previous_right_encoder_count
     global previous_left_encoder_count
-    # global left_angular_vel
-    # global right_angular_vel
-    global right_pos
-    global left_pos
+    global left_angular_vel
+    global right_angular_vel
     global vel_magnitude
-    # # left wheel angular velocity estimate
-    # left_angular_vel = ((2*PI*(left_encoder_count-previous_left_encoder_count))/(PULSES_PER_ROTATION*DT))
-    # # right wheel angular velocity estimate
-    # right_angular_vel = ((2*PI*(right_encoder_count-previous_right_encoder_count))/(PULSES_PER_ROTATION*DT))
-    # # updating previous encoder counts
-    # previous_left_encoder_count = left_encoder_count
-    # previous_right_encoder_count = right_encoder_count
-    # # velocity estimate
-    # vel_magnitude = (left_angular_vel+right_angular_vel)*WHEEL_RADIUS/2
-    # velocity_estimate_publisher.publish(vel_magnitude)
     # left wheel angular velocity estimate
-    
-    left_pos = ((2*PI*(left_encoder_count-previous_left_encoder_count))/(PULSES_PER_ROTATION))
+    left_angular_vel = ((2*PI*(left_encoder_count-previous_left_encoder_count))/(PULSES_PER_ROTATION*DT))
     # right wheel angular velocity estimate
-    right_pos = ((2*PI*(right_encoder_count-previous_right_encoder_count))/(PULSES_PER_ROTATION))
+    right_angular_vel = ((2*PI*(right_encoder_count-previous_right_encoder_count))/(PULSES_PER_ROTATION*DT))
     # updating previous encoder counts
     previous_left_encoder_count = left_encoder_count
     previous_right_encoder_count = right_encoder_count
     # velocity estimate
-    vel_magnitude = (left_pos+right_pos)*WHEEL_RADIUS/DT/2
+    vel_magnitude = (left_angular_vel+right_angular_vel)*WHEEL_RADIUS/2
     velocity_estimate_publisher.publish(vel_magnitude)
 
 def Velocity_Estimation_Node():
