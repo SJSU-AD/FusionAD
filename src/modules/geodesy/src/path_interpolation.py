b818a7adacac35b00555c83f624b3846f3be70db
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2018-12-07 11:19:28 -0800

    Revert "Iss 99 (#103)"
    This reverts commit ded75134d4847d49fb4e03150f2abbfa57b8b19f.

--- src/modules/geodesy/src/path_interpolation.py
+++ src/modules/geodesy/src/path_interpolation.py
@@ -19,7 +19,6 @@ def main():
     inputLatitudes, inputLongitudes, inputHeights = gps_parser.read_file_coarse_points(filePath, chosenHeight)
     
     conversionType = rospy.get_param("~conversion_type")
-    cmPerPoint = float(rospy.get_param("~cm_per_point"))
 
     if conversionType == "ECEF":
         interpolatorECEF = PathInterpolatorECEF(inputLatitudes, inputLatitudes, inputHeights)
@@ -30,7 +29,7 @@ def main():
         except rospy.ROSInterruptException:
             pass
     elif conversionType == "ENU":
-        interpolatorENU = PathInterpolatorENU(inputLatitudes, inputLongitudes, inputHeights, centimetersPerPoint=cmPerPoint)
+        interpolatorENU = PathInterpolatorENU(inputLatitudes, inputLongitudes, inputHeights, centimetersPerPoint=10)
         
         try:
             rospy.loginfo("ENU Conversion publishing...")
