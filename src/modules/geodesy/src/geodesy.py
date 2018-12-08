5ee5de74d977b81502f410a92957b5995615b12f
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2018-12-07 17:40:58 -0800

    Add read only access to constants

--- src/modules/geodesy/src/geodesy.py
+++ src/modules/geodesy/src/geodesy.py
@@ -17,11 +17,27 @@ class Geodesy(object):
     """
 
     def __init__(self):
-        self.a = 6378137.0         # equatorial radius of earth. Also the WGS-84 Earth semimajor axis in meters
-        self.b = 6356752.314245    # polar radius of earth. Also the derived Earth semiminor axis in meters
-        self.e = 0.08181788116     # eccentricity of the earth
-        self.f = (self.a - self.b) / self.a       # flattening coefficient. Also (1 / 298.257223563)
+        self._a = 6378137.0         # equatorial radius of earth. Also the WGS-84 Earth semimajor axis in meters
+        self._b = 6356752.314245    # polar radius of earth. Also the derived Earth semiminor axis in meters
+        self._e = 0.08181788116     # eccentricity of the earth
+        self._f = (self.a - self.b) / self.a       # flattening coefficient. Also (1 / 298.257223563)
         self.eSquared = self.f * (2 - self.f)      # square of eccentricity
+
+    @property
+    def a(self):
+        return self._a
+
+    @property
+    def b(self):
+        return self._b
+    
+    @property
+    def e(self):
+        return self._e
+
+    @property
+    def f(self):
+        return self._f
 
     def euclidian_distance_3d(self, x1, y1, z1, x2, y2, z2):
         """3-D euclidian distance between two points. Intended use in meters"""
