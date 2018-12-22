3b517abe9544662ec54ca00b85594aa010836590
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2018-12-21 22:52:45 -0500

    Remove unnecessary custom errors

--- src/modules/geodesy/src/permission_access.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""Exceptions to be thrown when client attempts to access unaccesible permissions"""
-
-class ReadOnlyError(Exception):
-    pass
-
-class UndeletablePropertyError(Exception):
-    pass
