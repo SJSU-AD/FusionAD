e5065ce3d9dc75b4544fc15b1ab5d9442be9a2c9
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2019-01-10 19:47:57 -0800

    Start bag parser

--- src/modules/geodesy/src/validate_enu_to_latlon.py
+++ src/modules/geodesy/src/validate_enu_to_latlon.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
-"""Convert ENU data to latitude/longitude GPSVisualizer data"""
+"""Convert ENU data to latitude/longitude GPSVisualizer data
+
+Accepts path to bagfile to parse. Converts ENU coordinates from path
+and GPS data from bag into latitude/longitude in '.csv' lat/lon format
+and '.txt' GPSVisualizer format.
+
+Data from the '.txt' file can then be plotted at http://www.gpsvisualizer.com/draw/
+
+Output files can be found in the /geodesy/geodesy_data/data_validation directory
+"""
 
 from __future__ import print_function
 from __future__ import division
@@ -8,6 +17,8 @@ from __future__ import division
 from Tkinter import Tk
 from tkFileDialog import askopenfilename
 import argparse
+
+import rosbag
 
 import validate_geodesy_conversion as gct
 
@@ -28,7 +39,7 @@ def prompt_input():
     """
 
     Tk().withdraw()
-    fileName = askopenfilename()
+    bagFilePath = askopenfilename()
     
     parser = argparse.ArgumentParser(description="Get file names")
     parser.add_argument("-f", "--frequency", 
@@ -50,12 +61,29 @@ def prompt_input():
     args = parser.parse_args()
     print("args are: ", args.filteringRate, args.conversionFileName, args.visualizerFileName)
 
-    return fileName, args.filteringRate, args.conversionFileName, args.visualizerFileName
+    return bagFilePath, args.filteringRate, args.conversionFileName, args.visualizerFileName
+
+def parse_bag(bagFilePath):
+    """Parses bag file for ENU data and writes to '.csv'"""
+
+    bag = rosbag.Bag(bagFilePath)
+    eData = []
+    nData = []
+
+    for topic, msg, t in bag.read_messages(topics=["/planning/trajectory"]):
+        for seq in range(len(msg.poses) - 1):
+            eData.append(msg.poses[seq].pose.position.x)
+            nData.append(msg.poses[seq].pose.position.x)
+    
+    # for topic, msg, t in bag.read_messages(topic=["/localization/state"]):
+    #     for seq in range(len(msg))
+    
+    bag.close()
 
 def main():
-    fileName, filteringRate, latLonFileNameCsv, latLonFileNameTxt = prompt_input()
+    bagFilePath, filteringRate, latLonFileNameCsv, latLonFileNameTxt = prompt_input()
 
-    print("fileName: ", fileName)
+    print("fileName: ", bagFilePath)
     print("args after are: ", filteringRate, latLonFileNameCsv, latLonFileNameTxt)
 
 if __name__ == "__main__":
