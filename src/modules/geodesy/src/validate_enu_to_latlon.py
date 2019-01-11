4914fc2cfbaf48dfa6985cafb67f26b1636d9b7a
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2019-01-10 19:22:52 -0800

    Add configurable params

--- src/modules/geodesy/src/validate_enu_to_latlon.py
+++ src/modules/geodesy/src/validate_enu_to_latlon.py
@@ -7,17 +7,57 @@ from __future__ import division
 
 from Tkinter import Tk
 from tkFileDialog import askopenfilename
+import argparse
 
-import geodesy_converter_tester as gct
+import validate_geodesy_conversion as gct
 
 # TODO: Input ENU data is mandatory
 # TODO: Output file names, freq are optional
 
-def main():
-    Tk().withdraw()
-    filename = askopenfilename()
-    print(filename)
+def prompt_input():
+    """Get necessary inputs for conversion
+    
+    Returns
+    -------
+    str
+        Path to input bagfile
+    str
+        Name of output lat/lon conversion file
+    str
+        Name of output lat/lon GPSVisualizer file
+    """
 
-if __name__ == '__main__':
+    Tk().withdraw()
+    fileName = askopenfilename()
+    
+    parser = argparse.ArgumentParser(description="Get file names")
+    parser.add_argument("-f", "--frequency", 
+                        help="Filtering frequency",
+                        default=100,
+                        type=int,
+                        dest="filteringRate")
+    parser.add_argument("-c", "--conversion-to-lat-long-file", 
+                        help="File name to output file storing converted lat/lon values in '.csv' format",
+                        default="",
+                        type=str,
+                        dest="conversionFileName")
+    parser.add_argument("-g", "--gpsvisualizer-format-file", 
+                        help="File name to output file storing converted lat/lon values in GPS visualizer format",
+                        default="",
+                        type=str,
+                        dest="visualizerFileName")
+
+    args = parser.parse_args()
+    print("args are: ", args.filteringRate, args.conversionFileName, args.visualizerFileName)
+
+    return fileName, args.filteringRate, args.conversionFileName, args.visualizerFileName
+
+def main():
+    fileName, filteringRate, latLonFileNameCsv, latLonFileNameTxt = prompt_input()
+
+    print("fileName: ", fileName)
+    print("args after are: ", filteringRate, latLonFileNameCsv, latLonFileNameTxt)
+
+if __name__ == "__main__":
     main()
 
