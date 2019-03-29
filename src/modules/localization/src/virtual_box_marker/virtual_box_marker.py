a93a4c265a5105e1cd6533c8280fcb095e7f8b23
Joey Zhu <joeyzhu00@gmail.com>
2019-03-29 13:55:16 -0700

    moved to base_link frame

--- src/modules/localization/src/virtual_box_marker/virtual_box_marker.py
+++ src/modules/localization/src/virtual_box_marker/virtual_box_marker.py
@@ -2,14 +2,7 @@
 
 """
 NOTE: Generates a rectangular box in front of the vehicle in rviz that represents the lidar virtual box. This is purely
-      for visualizing the lidar points that are in the virtual box. The code subscribes to the EKF output to get the 
-      current position of the vehicle and then generates a box based off of the parameters set by the user in the 
-      state_estimation.launch launch file. 
-
-      Subscribes
-      ----------
-      Topic: /odometry/filtered
-        Msg: nav_msgs/Odometry
+      for visualizing the lidar points that are in the virtual box. Uses the parameters in the virtual box launch file
 
       Publishes
       ---------
@@ -22,51 +15,82 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PolygonStamped, Point32
 
 class VirtualBox(object):
-    """Takes the odometry data from robot localization and publishes a virtual box"""
+    """Publishes a box in rviz from parameters in the virtual box launch file"""
 
     def __init__(self):
         """Initializing MarkerArray message publisher"""
-        self.virtualBoxPublisher = rospy.Publisher("/virtual_box_visualization", PolygonStamped, queue_size=1)
+        self.virtualBoxFrontPublisher = rospy.Publisher("/virtual_box_front_visualization", PolygonStamped, queue_size=1)
+        self.virtualBoxRearPublisher = rospy.Publisher("/virtual_box_rear_visualization", PolygonStamped, queue_size=1)
+        self.virtualBoxRightPublisher = rospy.Publisher("/virtual_box_right_visualization", PolygonStamped, queue_size=1)
+        self.virtualBoxLeftPublisher = rospy.Publisher("/virtual_box_left_visualization", PolygonStamped, queue_size=1)
+    
+    def vbox_callback(self, event):
+        """Timer callback for the polygon, creation @ 20 hz"""
+        self.create_virtual_box_base_link()
 
-    def odom_callback(self, odomMsg):
-        """Callback subscribed to the odometry message"""
-        # generate and publish the virtual box
-        create_virtual_box_poly(odomMsg.pose.pose.position.x, odomMsg.pose.pose.position.y)
+    def create_virtual_box_base_link(self):
+        """Compose and publish PolygonStamped message in the base_link frame"""
+        polygonFrontMsg = PolygonStamped()
+        polygonRearMsg = PolygonStamped()
+        polygonRightMsg = PolygonStamped()
+        polygonLeftMsg = PolygonStamped()
 
-    def create_virtual_box_poly(self, x, y):
-        """Compose and publish PolygonStamped message"""
-        # retrieve parameters from the launch file
-        vbox_x_lower_bound = rospy.get_param('/virtual_box_counter/x_lower_bound')
-        vbox_x_upper_bound = rospy.get_param('/virtual_box_counter/x_upper_bound')
+        # vbox_x_lower_bound = rospy.get_param('/virtual_box_counter/x_lower_bound')
+        # vbox_x_upper_bound = rospy.get_param('/virtual_box_counter/x_upper_bound')
         
-        vbox_y_lower_bound = rospy.get_param('/virtual_box_counter/y_lower_bound')
-        vbox_y_upper_bound = rospy.get_param('/virtual_box_counter/y_upper_bound')
+        # vbox_y_lower_bound = rospy.get_param('/virtual_box_counter/y_lower_bound')
+        # vbox_y_upper_bound = rospy.get_param('/virtual_box_counter/y_upper_bound')
 
-        vbox_z_lower_bound = rospy.get_param('/virtual_box_counter/z_lower_bound')
-        vbox_z_upper_bound = rospy.get_param('/virtual_box_counter/z_upper_bound')
+        # vbox_z_lower_bound = rospy.get_param('/virtual_box_counter/z_lower_bound')
+        # vbox_z_upper_bound = rospy.get_param('/virtual_box_counter/z_upper_bound')
 
-        # assemble the polygon message 
-        polygonMsg = PolygonStamped()
+        vbox_x_lower_bound = 0
+        vbox_x_upper_bound = 1
 
-        # generating the points for the rectangular prism is separated into two rectangles
-        # starting from the first rectangle closer to the vehicle at the bottom left corner and going clockwise
-        polygonMsg.polygon.points = [Point32(vbox_x_lower_bound + x, vbox_y_lower_bound + y, z_lower_bound),
-                                     Point32(vbox_x_lower_bound + x, vbox_y_lower_bound + y, z_upper_bound),
-                                     Point32(vbox_x_lower_bound + x, vbox_y_upper_bound + y, z_upper_bound),
-                                     Point32(vbox_x_lower_bound + x, vbox_y_upper_bound + y, z_lower_bound),
-                                     Point32(vbox_x_upper_bound + x, vbox_y_lower_bound + y, z_lower_bound),
-                                     Point32(vbox_x_upper_bound + x, vbox_y_lower_bound + y, z_upper_bound),
-                                     Point32(vbox_x_upper_bound + x, vbox_y_upper_bound + y, z_upper_bound),
-                                     Point32(vbox_x_upper_bound + x, vbox_y_upper_bound + y, z_lower_bound)]
+        vbox_y_lower_bound = -0.5
+        vbox_y_upper_bound = 0.5
+
+        vbox_z_lower_bound = 0
+        vbox_z_upper_bound = 1
+
+        polygonFrontMsg.polygon.points = [Point32(vbox_x_lower_bound, vbox_y_lower_bound, vbox_z_lower_bound),
+                                          Point32(vbox_x_lower_bound, vbox_y_lower_bound, vbox_z_upper_bound),
+                                          Point32(vbox_x_lower_bound, vbox_y_upper_bound, vbox_z_upper_bound),
+                                          Point32(vbox_x_lower_bound, vbox_y_upper_bound, vbox_z_lower_bound)]
         
-        polygonMsg.frame_id = "odom"
-        polygonMsg.stamp = rospy.Time.now()
+        polygonRearMsg.polygon.points = [Point32(vbox_x_upper_bound, vbox_y_lower_bound, vbox_z_lower_bound),
+                                         Point32(vbox_x_upper_bound, vbox_y_lower_bound, vbox_z_upper_bound),
+                                         Point32(vbox_x_upper_bound, vbox_y_upper_bound, vbox_z_upper_bound),
+                                         Point32(vbox_x_upper_bound, vbox_y_upper_bound, vbox_z_lower_bound)]
 
-        self.virtualBoxPublisher.publish(polygonMsg)
+        polygonRightMsg.polygon.points = [Point32(vbox_x_lower_bound, vbox_y_upper_bound, vbox_z_upper_bound),
+                                          Point32(vbox_x_lower_bound, vbox_y_upper_bound, vbox_z_lower_bound),
+                                          Point32(vbox_x_upper_bound, vbox_y_upper_bound, vbox_z_lower_bound),
+                                          Point32(vbox_x_upper_bound, vbox_y_upper_bound, vbox_z_upper_bound)]
+        
+        polygonLeftMsg.polygon.points = [Point32(vbox_x_lower_bound, vbox_y_lower_bound, vbox_z_lower_bound),
+                                         Point32(vbox_x_lower_bound, vbox_y_lower_bound, vbox_z_upper_bound),
+                                         Point32(vbox_x_upper_bound, vbox_y_lower_bound, vbox_z_upper_bound),
+                                         Point32(vbox_x_upper_bound, vbox_y_lower_bound, vbox_z_lower_bound)]
+
+        polygonFrontMsg.header.frame_id = "base_link"
+        polygonRearMsg.header.frame_id = "base_link"
+        polygonRightMsg.header.frame_id = "base_link"
+        polygonLeftMsg.header.frame_id = "base_link"
+
+        polygonFrontMsg.header.stamp = rospy.Time.now()
+        polygonRearMsg.header.stamp = rospy.Time.now()
+        polygonRightMsg.header.stamp = rospy.Time.now()
+        polygonLeftMsg.header.stamp = rospy.Time.now()
+
+        self.virtualBoxFrontPublisher.publish(polygonFrontMsg)
+        self.virtualBoxRearPublisher.publish(polygonRearMsg)
+        self.virtualBoxRightPublisher.publish(polygonRightMsg)
+        self.virtualBoxLeftPublisher.publish(polygonLeftMsg)
 
     def virtual_box_creation(self):
-        """Take the EKF odom data, create a polygon message and publish to rviz for visualization"""
-        rospy.Subscriber("/odometry/filtered", Odometry, self.odom_callback)
+        """Create a polygon message and publish to rviz for visualization"""
+        rospy.Timer(rospy.Duration(0.05), self.vbox_callback)
         rospy.spin()
 
 def main():
