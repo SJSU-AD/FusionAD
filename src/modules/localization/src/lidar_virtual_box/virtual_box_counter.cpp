425e1e775ea986d4970135975edd5d15946ed099
Joey Zhu <joeyzhu00@gmail.com>
2019-04-04 19:34:00 -0700

    Iss 236 (#238)
    * ISS 123 (#135)
    
    * Iss 120 (#121)
    
    * Refactored Localization folders and made (potential) gpf fix
    
    * Fixed file endings
    
    * Working on updating normal calculation method
    
    * New normal estimation method
    
    * New Gpf method
    
    * Gpf final cleanup and stable build
    
    * Added comments to segmenter_node
    
    * Kalman Filter Code
    
    * Removed unused Kalman Filter code
    
    * Moving Median Filter Wheel Odometry (#117)
    
    * yes
    
    yes
    
    * Switched control to run on imu yaw, implemented tf from imu frame to ENU w/ consideration on magnetic declination
    
    * Magnetic_declination angle fix
    
    * Build Error Fix
    
    * Changed imu tf from 105 deg to 90 deg
    
    * Updated ardu_adapter.py structure and included TODO to address subscriber frequency issue with steering operation
    
    * Update changes
    
    * Adding Wheel Odometry node along with velocity covariance collection python script. Arduino code included for getting data from SIGNWISE 600 pulse/rev quadrature encoder. Monitoring change in pulse for interrupts for four times the resolution (2400 pulse/rev).
    
    * Update src/modules/control/src/ardu_adapter.py
    
    Co-Authored-By: joeyzhu00 <joeyzhu00@gmail.com>
    
    * Added Loam and test_gsl into repo
    
    * Editing the ext_package_build.sh file
    
    * Fixing commenting error in the ardu_adapter.py code and inclusion of robot_localization package in the utility folder
    
    * Removed Robot local pkg
    
    * Putting robot_localization pkg back in
    
    * Iss 104 (#112)
    
    * Switched control to run on imu yaw, implemented tf from imu frame to ENU w/ consideration on magnetic declination
    
    * Magnetic_declination angle fix
    
    * Build Error Fix
    
    * Added new installation steps to bash script, added new message def for planning
    
    * Naming convention Clean up for control stack, revised debugging data collection mechanism
    
    * Update src/modules/interface/msg/Waypoint.msg
    
    Co-Authored-By: menson168 <menson168@gmail.com>
    
    * Fixed zero gain bug, tested in unit test
    
    * Changed Test code to using near for decimal prceision, Test ran and passed all cases
    
    * Modify localization support scripts
    
    * Including a moving median filter to the wheel odometry
    
    * Adding support script for the EKF package to conform to ROS sensor_msgs standards in a yaw=0 pointing East ENU frame.
    
    * Cleaning up file names
    
    * Changing file names and limited the serial bus load in terms of bit rate by changing data types used
    
    * Changing Moving Median Filter to Median Filter
    
    * Work in progress for ISS-123
    
    * Addition of the yaw calibration to the Wheel Odometry code
    
    * Revert "ISS 123 (#135)" (#136)
    
    This reverts commit 00b1ebb4e66d030003c2d606720bad42a61c4a38.
    
    * Updated geodesy subscriber and added more comments to the odometry code
    
    * Initial commit of the frame_calibration node. Need to add tf listener and transform logic as well as a separate tf broadcaster node
    
    * Refactored frame_calibration node (thanks Francisco) as well as initial commit of tf2 broadcaster in the master_transforms module
    
    * Initial TF class implementation to tf broadcast and tf listener logic between master_tf and frame_calibration
    
    * Removed frame placeholder for tf broadcaster and put in actual frame id
    
    * GPS transform addition works with EKF according the most recent bag
    
    * Initial commit of the wheel odometry dead-reckoning, confirmed to work with EKF
    
    * Added necessary comments and clarification for each script
    
    * Refactored the localization module and removed imu_tf_adapter.cpp
    
    * addresses ISS-155
    
    * Comment style consistency
    
    * Included some clarification in comments
    
    * initial commit to ISS-236
    
    * added more code
    
    * moved package to localization, no more dependency errors
    
    * currently have runtime error
    
    * resolved runtime error
    
    * removed confusing argument in for loop
    
    * changed launch file for tuning
    
    * initial commit ISS-237
    
    * fixed some bugs
    
    * moved to base_link frame
    
    * verified with virtual box
    
    * changed frame_id

---
+++ src/modules/localization/src/lidar_virtual_box/virtual_box_counter.cpp
@@ -0,0 +1,85 @@
+#include "localization/lidar_virtual_box/virtual_box_counter.h"
+
+namespace fusionad
+{
+namespace localization
+{
+namespace pc_processing_node
+{
+
+    PcProcessingNode::PcProcessingNode(){}
+
+    PcProcessingNode::~PcProcessingNode(){}
+
+    void PcProcessingNode::initRosComm()
+    {
+        // publisher for object detected within virtual box
+        cluster_pub = pcProcessingNode_nh.advertise<std_msgs::Bool>("/perception/point_cloud_detection", 10);
+        pc_pub = pcProcessingNode_nh.advertise<sensor_msgs::PointCloud>("/localization/pointcloud_msg", 10);
+
+        // subscriber to the lidar
+        lidar_sub = pcProcessingNode_nh.subscribe("/velodyne_points", 10, &PcProcessingNode::lidarCallback, this);
+    }
+
+    void PcProcessingNode::lidarCallback(const sensor_msgs::PointCloud2& lidar_msg)
+    {
+        // pointcloud msg to parse xyz position of points
+        sensor_msgs::PointCloud out_cloud;
+        sensor_msgs::convertPointCloud2ToPointCloud(lidar_msg, out_cloud);
+        
+        pc_pub.publish(out_cloud);
+
+        // get virtual box parameters from launch file
+        float x_lower_bound, x_upper_bound, y_lower_bound, y_upper_bound, z_lower_bound, z_upper_bound;
+
+        pcProcessingNode_nh.getParam("/virtual_box_counter/x_lower_bound", x_lower_bound);
+        pcProcessingNode_nh.getParam("/virtual_box_counter/x_upper_bound", x_upper_bound);
+
+        pcProcessingNode_nh.getParam("/virtual_box_counter/y_lower_bound", y_lower_bound);
+        pcProcessingNode_nh.getParam("/virtual_box_counter/y_upper_bound", y_upper_bound);
+
+        pcProcessingNode_nh.getParam("/virtual_box_counter/z_lower_bound", z_lower_bound);
+        pcProcessingNode_nh.getParam("/virtual_box_counter/z_upper_bound", z_upper_bound);
+
+        unsigned int points_in_box = 0;
+        
+        for(int i = 0; i < out_cloud.points.size(); i++)
+        {
+            // check for the # of points within the virtual box
+            if((out_cloud.points[i].x >= x_lower_bound) && (out_cloud.points[i].x <= x_upper_bound) && 
+               (out_cloud.points[i].y >= y_lower_bound) && (out_cloud.points[i].y <= y_upper_bound) && 
+               (out_cloud.points[i].z >= z_lower_bound) && (out_cloud.points[i].z <= z_upper_bound))
+               {
+                   points_in_box++;
+               }
+        }
+
+        // if the points in box threshold has been met
+        int points_in_box_threshold;
+        pcProcessingNode_nh.getParam("/virtual_box_counter/points_in_box_threshold", points_in_box_threshold);
+
+        if(points_in_box >= points_in_box_threshold)
+        {
+            std_msgs::Bool points_exceeded_threshold;
+            points_exceeded_threshold.data = true;
+            cluster_pub.publish(points_exceeded_threshold);
+        }
+    }
+}// pc_processing_node
+}// localization
+}// fusionad
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "virtual_box_counter");
+    fusionad::localization::pc_processing_node::PcProcessingNode virtual_box_counter;
+
+    virtual_box_counter.initRosComm();
+
+    while(ros::ok())
+    {
+        ros::spinOnce();
+    }
+
+    return 0;
+}
